8eab9283874b6493d9c25a796253f172019b7690
Eddy Jaga <eddyjaga@gmail.com>
2022-05-23 22:45:01 +1000

    Change user login design and sigup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'signup' }
   resources :friends
   get "about", to: "home#about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
